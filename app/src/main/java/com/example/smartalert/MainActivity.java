c355364495efb3fdb94e2f343dab654691075120
Giorgos Seimenis <giorgosseimenis@gmail.com>
2023-02-13 18:05:49 +0200

    added activities to manifest

--- app/src/main/java/com/example/smartalert/MainActivity.java
+++ app/src/main/java/com/example/smartalert/MainActivity.java
@@ -5,23 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
     }
 
     // Login button
-    public void login(View view){
-        Intent intent = new Intent(this,User_Profile.class);
+    public void Login(View view)
+    {
+        Intent intent = new Intent(this, User_Profile.class);
         startActivity(intent);
+        finish();
     }
 
     // Go to register page, if user does not have an account already
-    public void go_to_register(View view){
+    public void go_to_register(View view)
+    {
         Intent intent = new Intent(this,Register.class);
         startActivity(intent);
     }
