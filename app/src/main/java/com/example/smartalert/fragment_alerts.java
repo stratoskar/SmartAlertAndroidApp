8b17c51a99eab288d28e9de8c2e5d432a53b0d85
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2023-02-17 18:57:57 +0200

    Finished Cards

--- app/src/main/java/com/example/smartalert/fragment_alerts.java
+++ app/src/main/java/com/example/smartalert/fragment_alerts.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.fragment.app.Fragment;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,4 +20,21 @@ public class fragment_alerts extends Fragment {
                              @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_alerts, container, false);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    /* Adds a new card to the screen
+    public addCard()
+    {
+        View view = getLayoutInflater().inflate(R.layout.card,null);
+        TextView text = view.findViewById(R.id.textView);
+        ImageView image = view.findViewById(R.id.imageView);
+        Button accept_button = view.findViewById(R.id.Accept_Button);
+        Button decline_button = view.findViewById(R.id.Decline_Button);
+    }
+    */
 }
