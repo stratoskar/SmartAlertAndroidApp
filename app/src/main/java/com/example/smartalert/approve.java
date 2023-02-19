c0dc62ed9139964bdfa7d86b80d6c0fb3d9d0cae
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2023-02-19 19:02:24 +0200

    Έτοιμη η κλάση για το approve

---
+++ app/src/main/java/com/example/smartalert/approve.java
@@ -0,0 +1,18 @@
+package com.example.smartalert;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.smartalert.R;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class approve extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        // start activity
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.approve);
+    }
+
