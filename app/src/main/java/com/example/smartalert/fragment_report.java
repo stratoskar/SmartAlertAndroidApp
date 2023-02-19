394fcf56d08bda5d9c5881424253bc950f2bd68d
Efstratios Karkanis <stratoskarkanis2@gmail.com>
2023-02-19 16:54:15 +0200

    Τσεκάρω αν στην reports είναι συμπληρωμένα όλα. Έπειτα πάει στην fragment approve

--- app/src/main/java/com/example/smartalert/fragment_report.java
+++ app/src/main/java/com/example/smartalert/fragment_report.java
@@ -1,27 +1,74 @@
 package com.example.smartalert;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 public class fragment_report extends Fragment
 {
+
+    private EditText description;
+    private RadioButton radioButton;
+    private RadioGroup radioGroup;
+
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false);
+        View view = inflater.inflate(R.layout.fragment_report, container, false);
+        ConstraintLayout layout = (ConstraintLayout) view;
+
+        radioGroup = layout.findViewById(R.id.radio_radioGroup);
+        description = layout.findViewById(R.id.editText_enter_description);
+
+       return view;
     }
+
+    // check if a radio button value is selected by the user or not
+    public String takeRadioButtonValue() {
+        int selectedId = radioGroup.getCheckedRadioButtonId();
+        radioButton = radioButton.findViewById(selectedId);
+        if (selectedId == -1) {
+            return "Nothing Selected";
+        }
+        else
+        {
+            return "OK";
+        }
+    }
+
+    // this method is called as soon as the button is clicked by the user
+    public void checkIfEveryFieldIsFilled(LayoutInflater inflater, ViewGroup container)
+    {
+        String radioValue = takeRadioButtonValue();
+        String descriptionValue = description.getText().toString();
+        if (descriptionValue == "" || radioValue == "Nothing Selected")
+        {
+            View view = inflater.inflate(R.layout.fragment_approve, container, false);
+            //return View;
+        }
+        else
+        {
+            // show message to user
+        }
+    }
+
 }
